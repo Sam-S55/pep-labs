8fe6526d38c2ee795da9797a0ebb5501c34f7fb8
Sam-S55 <swai554m@gmail.com>
2022-12-21 22:05:13 +0000

    Saved progress on Math 2022-12-21 22:05:13 +0000

--- Math/src/main/java/MathClass.java
+++ Math/src/main/java/MathClass.java
@@ -1,3 +1,4 @@
+import static java.lang.Math.*;
 
 public class MathClass {
     /**
@@ -20,6 +21,6 @@ public class MathClass {
      * @return return a^b using the pow() method of the Math class.
      */
     public double mathPow(double a, double b){
-        return 0;
+        return pow(a,b);
     }
 }
