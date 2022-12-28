4c9313fbdf3a136e501028a5934738dee3f863c8
Sam-S55 <swai554m@gmail.com>
2022-12-28 22:10:29 +0000

    Saved progress on UncheckedException 2022-12-28 22:10:29 +0000

--- UncheckedException/src/main/java/CauseUncheckedException.java
+++ UncheckedException/src/main/java/CauseUncheckedException.java
@@ -14,6 +14,7 @@ public class CauseUncheckedException {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
-
+        int i;
+        i = 23/0;
     }
 }
