b3503ccb301139fbb147bbf1b1d96f3e507c59fb
Sam-S55 <swai554m@gmail.com>
2022-12-29 23:00:51 +0000

    Saved progress on Map 2022-12-29 23:00:51 +0000

--- Map/src/main/java/MapExercise.java
+++ Map/src/main/java/MapExercise.java
@@ -1,5 +1,6 @@
 
 import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Maps are key/value pairs. They are used in situations where we have some value that we'd like
@@ -25,8 +26,9 @@ public class MapExercise {
      * HashMap and TreeMap. Also like the other collections, Map must leverage a wrapper class instead of a primitive,
      * so here Integer is a wrapper for int.
      */
-    public Map<Integer, String> createMap(){
-        return null;
+    public Map<Integer,String> createMap(){
+        Map<Integer,String> map = new HashMap<Integer,String>();
+        return map;
     }
 
     /**
@@ -36,7 +38,7 @@ public class MapExercise {
      */
     public int getSize(Map<Integer,String> map){
 //        return total number of key/value pairs in the map
-        return 0;
+        return map.size();
     }
 
     /**
@@ -47,6 +49,7 @@ public class MapExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addKeyValuePair(Map<Integer,String> map, int key, String value){
+        map.put(key,value);
     }
 
     /**
@@ -56,7 +59,7 @@ public class MapExercise {
      * @return the value associated with key in map.
      */
     public String getValueFromKey(Map<Integer, String> map, int key){
-        return "";
+        return map.get(key);
     }
 
     /**
@@ -66,6 +69,7 @@ public class MapExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeKeyValuePair(Map<Integer, String> map, int key){
+        map.remove(key);
     }
 
     /**
@@ -76,6 +80,8 @@ public class MapExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void overwriteValue(Map<Integer, String> map, int key, String value){
-
+        if(map.containsKey(key)){
+            map.put(key, value);
+        }
     }
 }
