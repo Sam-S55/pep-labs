e9d4904e3e7f6f56fd131a1580a843b38eaa31a1
Sam-S55 <swai554m@gmail.com>
2023-01-26 21:10:00 +0000

    Saved progress on MostCommonCharacter 2023-01-26 21:10:00 +0000

--- MostCommonCharacter/src/main/java/MostCommonCharacter.java
+++ MostCommonCharacter/src/main/java/MostCommonCharacter.java
@@ -1,3 +1,5 @@
+import java.util.*;
+import java.util.Map.Entry;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +10,22 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        char common = ' ';
+        Map<Character,Integer> count = new HashMap<Character,Integer>();
+        for (int i=0; i<str.length(); i++) { //add neccesary characters to map
+            count.put(str.charAt(i), 0);
+        }
+        
+        for (int i=0; i<str.length(); i++) { //tally up the number of occurences for each character
+            count.put(str.charAt(i), count.get(str.charAt(i))+1);
+        }
+
+        int maxvalue = Collections.max(count.values());
+        for (Entry<Character, Integer> entry : count.entrySet()) {
+            if (entry.getValue() == maxvalue) {
+                common = entry.getKey();
+            }
+        }
+        return common;
     }
 }
